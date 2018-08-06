28149ee1be24b410d74983ed5201bc83f1c4d071
Duane Reinhardt <40170594+DwReinhardt@users.noreply.github.com>
2018-08-05 23:45:23 -0400

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,29 +1,4 @@
-alphabet_lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-alphabet_upper = ["A", "B", "B", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-
-def alphabet_position(letter):
-    tmp = letter.lower()
-    return(alphabet_lower.index(tmp))
-    
-def rotated_char(index, case):
-    if case == "lower":
-        return(alphabet_lower[index])
-    else:
-        return(alphabet_upper[index])
-
-
-def rotate_character(char, rot):
-    if char.isalpha() == False:
-        return char
-    case = "lower"
-    if char.isupper() == True:
-        case = "upper"
-    rotated_value = alphabet_position(char) + rot # rotate by rot
-    if rotated_value > 25:
-        rotated_value = rotated_value % 26
-    answer = rotated_char(rotated_value, case)
-    return answer
+from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
     encrypted = ""
@@ -33,10 +8,13 @@ def encrypt(text, rot):
         i += 1
         middle_step = rotate_character(tmp, rot)
         encrypted = encrypted + middle_step
-    return encrypted
+    return print("your encrypted message is: ", encrypted)
 
 def main():
-    encrypt("Hello, World!", 5)
- 
+    #encrypt(input("what message would you like to encrypt?\n"), int(input("how far would you like to rotate it?\n")))
+    string = input("what message would you like to encrypt?\n")
+    rot = int(input("how far would you like to rotate it?\n"))
+    encrypt(string, rot)
+
 if __name__ == "__main__":
     main()
